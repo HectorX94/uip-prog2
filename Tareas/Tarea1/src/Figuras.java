62529bc12e337711dfcb2d6dcbb6f023a79e838c
HectorX94 <hectorbreg94@gmail.com>
2015-10-15 00:36:15 -0500

    Tarea de Eclipse

---
+++ Tareas/Tarea1/src/Figuras.java
@@ -0,0 +1,9 @@
+
+public class Figuras {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Tareas/Tarea1/src/Poligono.java
@@ -0,0 +1,22 @@
+public class Poligono extends Figuras {
+	private Object Coordenada;
+	private Object x;
+	private Object y;
+	
+	
+	public void setx() {
+	
+	}
+	
+	public void sety() {
+	
+	}
+	
+	public void getx() {
+	
+	}
+	
+	public void gety() {
+	
+	}
+}
